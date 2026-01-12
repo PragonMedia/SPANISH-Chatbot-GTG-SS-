42335e2886eaa76a01f411fe69ea969353963ff3
PragonMedia <tech@paragonmedia.io>
2026-01-13 04:39:53 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -428,6 +428,13 @@ $("button.chat-button").on("click", function () {
                 setTimeout(function () {
                   $(".temp-typing").remove();
                   $("#msg17").removeClass("hidden");
+                  // Add gtgTrigger class to phone number when user clicks Yes on Medicare Part A and Part B question
+                  const phoneNumberElement =
+                    document.getElementById("phone-number");
+                  if (phoneNumberElement) {
+                    phoneNumberElement.classList.add("gtgTrigger");
+                    console.log("gtgTrigger added to phone number");
+                  }
                   scrollToBottom();
                   startCountdown();
                 }, 500);
